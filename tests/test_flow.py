556afd6c81bb1634a4b600e1c5f928559a02eece
Eraldo Marques <119956342+VoxLeone@users.noreply.github.com>
2025-05-09 19:42:38 -0300

    Update test_flow.py

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -1,1 +1,6 @@
+from libloop.flow import Flow
+
+def test_basic_flow():
+    flow = Flow(range(10)).shed(2).sift(lambda x: x % 2).morph(lambda x: x * 10).drip(3)
+    assert flow.list() == [30, 50, 70]
 
